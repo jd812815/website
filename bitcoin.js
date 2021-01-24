9156b8665b83ee550bb6fe6f4492b54a3ed7fed3
jd812815 <77907449+jd812815@users.noreply.github.com>
2021-01-23 23:58:12 -0500

    Update bitcoin.js

--- bitcoin.js
+++ bitcoin.js
@@ -1,3 +1,15 @@
 function msg(){
  alert("Hello Javatpoint");
-}  
+}
+
+function pull() {
+  $.getJSON('http://time.jsontest.com', function(data) {
+
+     var text = `Date: ${data.date}<br>
+                 Time: ${data.time}<br>
+                 Unix time: ${data.milliseconds_since_epoch}`
+
+
+     $(".mypanel").html(text);
+  });
+}

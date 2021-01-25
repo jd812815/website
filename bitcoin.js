744cd1c2f6d5b0d6e2ac5fed4a32bbb09ef0af9a
jd812815 <77907449+jd812815@users.noreply.github.com>
2021-01-25 01:26:10 -0500

    Update bitcoin.js

--- bitcoin.js
+++ bitcoin.js
@@ -1,14 +1,3 @@
 function msg(){
  alert("Hello Javatpoint");
 }}
-
-function pull() {
-  var coinInfo = "https://api.coingecko.com/api/v3/exchange_rates";
-  $.getJSON( coinInfo, function(json){
-    data = json;
-  }
-)}
-
-function log() {
-  console.log(data)
-}
